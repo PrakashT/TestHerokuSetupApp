0f87352e05f0105cc03c3dd4876837c2cb7e2555
People Tech <peopletech@Peoples-Mac-mini.local>
2018-11-28 15:38:04 +0530

    first commit

---
+++ TestHerokuSetupApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHerokuSetupApp
+//
+//  Created by People Tech on 28/11/18.
+//  Copyright © 2018 PTG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
